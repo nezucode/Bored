b435027f3f073bcc46c90614af60bfa93d9434b7
nezucode <intann89.in@gmail.com>
2023-05-08 16:03:33 +0700

    adding UI Alert Invalid Email

--- Bored/Controller/AlertManager.swift
+++ Bored/Controller/AlertManager.swift
@@ -5,4 +5,22 @@
 //  Created by Intan on 08/05/23.
 //
 
-import Foundation
+import UIKit
+
+class AlertManager {
+    private static func showBasicAlert(on vc: UIViewController, title: String, message: String?){
+        
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "title", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            vc.present(alert, animated: true)
+        }
+    }
+}
+
+//MARK: - Show Validation Alert
+extension AlertManager {
+    public static func showInvalidEmail(on vc: UIViewController){
+        self.showBasicAlert(on: vc, title: "Invalid Email", message: "Please enter a valid email address")
+    }
+}
