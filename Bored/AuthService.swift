3103f38f6779c32833dbea687a2e40812fb82bdf
nezucode <intann89.in@gmail.com>
2023-05-10 16:09:51 +0700

    Adding AuthUser

--- Bored/AuthService.swift
+++ Bored/AuthService.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class AuthService {
     
@@ -17,12 +19,42 @@ class AuthService {
     /// - Parameters:
     ///   - userRequest: The user information (username, email, password)
     ///   - completion: A completion with two values...
-    ///   - Bool: wasRegistered - Determines if the
+    ///   - Bool: wasRegistered - Determines if the the user was registered and saved in the database correctly
+    ///   - Error?: An optional error if firebase provides once
     public func registerUser(with userRequest: RegisterUserRequest,
                              completion: @escaping (Bool, Error?)->Void){
         let username = userRequest.username
         let email =  userRequest.email
         let password = userRequest.password
+        
+        Auth.auth().createUser(withEmail: email, password: password, completion: { result,
+            error in
+            if let error = error {
+                completion(false, error)
+                return
+            }
+            
+            guard let resultUser = result?.user else {
+                completion(false, nil)
+                return
+            }
+            
+            let db = Firestore.firestore()
+            
+            db.collection("users")
+                .document(resultUser.uid)
+                .setData([
+                    "username": username,
+                    "email": email
+                ]) { error in
+                    if let error = error {
+                        completion(false, error)
+                        return
+                    }
+                    
+                    completion(true, nil)
+                }
+        })
         
     }
     
