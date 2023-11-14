592006b5e358065f9b168f7666eb617411f35b93
nezucode <intann89.in@gmail.com>
2023-11-14 15:56:25 +0700

    delete forgot password button

--- Bored/Controller/LoginController.swift
+++ Bored/Controller/LoginController.swift
@@ -28,7 +28,6 @@ class LoginController: UIViewController {
         
         self.signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
         self.newUserButton.addTarget(self, action: #selector(didTapNewUser), for: .touchUpInside)
-        self.forgotPasswordButton.addTarget(self, action: #selector(didTapForgotPassword), for: .touchUpInside)
     }
     
     
@@ -46,14 +45,12 @@ class LoginController: UIViewController {
         self.view.addSubview(passwordField)
         self.view.addSubview(signInButton)
         self.view.addSubview(newUserButton)
-        self.view.addSubview(forgotPasswordButton)
         
         headerView.translatesAutoresizingMaskIntoConstraints = false
         emailField.translatesAutoresizingMaskIntoConstraints = false
         passwordField.translatesAutoresizingMaskIntoConstraints = false
         signInButton.translatesAutoresizingMaskIntoConstraints = false
         newUserButton.translatesAutoresizingMaskIntoConstraints = false
-        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             self.headerView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor),
@@ -79,12 +76,7 @@ class LoginController: UIViewController {
             self.newUserButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 14),
             self.newUserButton.centerXAnchor.constraint(equalTo: signInButton.centerXAnchor),
             self.newUserButton.heightAnchor.constraint(equalToConstant: 32),
-            self.newUserButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
-            
-            self.forgotPasswordButton.topAnchor.constraint(equalTo: newUserButton.bottomAnchor, constant: 4),
-            self.forgotPasswordButton.centerXAnchor.constraint(equalTo: newUserButton.centerXAnchor),
-            self.forgotPasswordButton.heightAnchor.constraint(equalToConstant: 24),
-            self.forgotPasswordButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85)
+            self.newUserButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85)
         ])
     }
     
